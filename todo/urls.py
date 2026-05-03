b29777c7921534d38e15a5c1a3441205778037d3
Mohammed Naveen <navsdumbs@gmail.com>
2026-05-03 21:11:12 +0530

    Finished the MVP

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/',views.addTask, name='addTask')
+    path('addTask/',views.addTask, name='addTask'),
+    path('deleteTask/<int:pk>/',views.deleteTask, name='deleteTask'),
+    path('editTask/<int:pk>/', views.editTask, name='editTask'),
+    path('markAsDone/<int:pk>/',views.markAsDone, name='markAsDone'),
+    path('markAsUndone/<int:pk>/', views.markAsUndone, name='markAsUndone'),
 ]
